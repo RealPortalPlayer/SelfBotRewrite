af07a15f2266a5c1531fea93a8f9dfcea97eaf4d
portalplayer <portalplayer@crime.su>
2024-04-22 20:23:39 -0400

    Project: Initialized

---
+++ source/Main.c
@@ -0,0 +1,9 @@
+// Copyright (c) 2024, PortalPlayer <email@portalplayer.xyz>
+// Licensed under MIT <https://opensource.org/licenses/MIT>
+
+#include <stdio.h>
+
+int main(void) {
+    printf("Hello, World!\n");
+    return 0;
+}
