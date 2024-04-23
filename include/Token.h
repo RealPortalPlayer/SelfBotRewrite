1f041986fd154b3596f2ac51b50696bb4d997b21
portalplayer <portalplayer@crime.su>
2024-04-22 20:28:31 -0400

    Project: Added settings, and basic token code

---
+++ include/Token.h
@@ -0,0 +1,9 @@
+// Purpose: Bot token management
+// Created on: 4/22/24 @ 8:26 PM
+
+// Copyright (c) 2024, PortalPlayer <email@portalplayer.xyz>
+// Licensed under MIT <https://opensource.org/licenses/MIT>
+
+#pragma once
+
+const char* SBR_Token_Get(void);
---
+++ source/Token.c
@@ -0,0 +1,8 @@
+// Copyright (c) 2024, PortalPlayer <email@portalplayer.xyz>
+// Licensed under MIT <https://opensource.org/licenses/MIT>
+
+#include "Token.h"
+
+const char* SBR_Token_Get(void) {
+    return "";
+}
