c5046874f7585ff5bf3dbbde65b5755cfc847437
portalplayer <portalplayer@crime.su>
2024-04-23 21:24:35 -0400

    Bot: Moved cURL message above starting curl

--- source/Main.c
+++ source/Main.c
@@ -1,7 +1,6 @@
 // Copyright (c) 2024, PortalPlayer <email@portalplayer.xyz>
 // Licensed under MIT <https://opensource.org/licenses/MIT>
 
-#include <stdio.h>
 #include <BaconAPI/ArgumentHandler.h>
 #include <BaconAPI/Logger.h>
 
@@ -11,12 +10,12 @@
 int main(int argc, char** argv) {
     BA_ArgumentHandler_Initialize(argc, argv);
     SBR_Settings_Load();
-    BA_LOGGER_INFO("Starting curl\n");
     BA_LOGGER_DEBUG("Discord:\n"
                     "API version: %i\n"
                     "API URL: %s\n"
                     "WebSocket URL: %s\n"
                     "libcurl: %s\n", DISCORD_API_VERSION, DISCORD_API_URL, DISCORD_WEBSOCKET_URL, curl_version());
+    BA_LOGGER_INFO("Starting curl\n");
     SBR_cURL_Initialize();
     return 0;
 }
