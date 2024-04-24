d833186b546a1a5f7ef92c94d7dbe42caf655e2f
portalplayer <portalplayer@crime.su>
2024-04-23 20:43:27 -0400

    Bot: Added debug version/URL information

--- source/Main.c
+++ source/Main.c
@@ -12,6 +12,11 @@ int main(int argc, char** argv) {
     BA_ArgumentHandler_Initialize(argc, argv);
     SBR_Settings_Load();
     BA_LOGGER_INFO("Starting curl\n");
+    BA_LOGGER_DEBUG("Discord:\n"
+                    "API version: %i\n"
+                    "API URL: %s\n"
+                    "WebSocket URL: %s\n"
+                    "libcurl: %s\n", DISCORD_API_VERSION, DISCORD_API_URL, DISCORD_WEBSOCKET_URL, curl_version());
     SBR_cURL_Initialize();
     return 0;
 }
