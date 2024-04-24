b86525de69d4248f592de12942ea3a94c565c823
portalplayer <portalplayer@crime.su>
2024-04-24 18:54:42 -0400

    Bot: Added user agent

---
+++ include/UserAgent.h
@@ -0,0 +1,9 @@
+// Purpose: Bot user agnet 
+// Created on: 4/24/24 @ 6:47 PM
+
+// Copyright (c) 2024, PortalPlayer <email@portalplayer.xyz>
+// Licensed under MIT <https://opensource.org/licenses/MIT>
+
+#pragma once
+
+const char* SBR_UserAgent_Get(void);
---
+++ source/UserAgent.c
@@ -0,0 +1,20 @@
+// Copyright (c) 2024, PortalPlayer <email@portalplayer.xyz>
+// Licensed under MIT <https://opensource.org/licenses/MIT>
+
+#include <BaconAPI/String.h>
+#include <curl/curl.h>
+
+#include "UserAgent.h"
+
+
+const char* SBR_UserAgent_Get(void) {
+    static char* userAgent = NULL;
+
+    if (userAgent == NULL) {
+        userAgent = BA_String_Copy("SelfBotRewrite/1.0.0-incomplete ");
+
+        BA_String_Append(&userAgent, curl_version());
+    }
+
+    return userAgent;
+}
