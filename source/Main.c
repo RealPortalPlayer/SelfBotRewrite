88003cba4202d136a461218eb666cf8690644476
portalplayer <portalplayer@crime.su>
2024-04-25 12:38:44 -0400

    Bot: Added help message

--- source/Main.c
+++ source/Main.c
@@ -6,9 +6,18 @@
 
 #include "Settings.h"
 #include "WebSocket/cURL.h"
+#include "BuiltInArguments.h"
 
 int main(int argc, char** argv) {
     BA_ArgumentHandler_Initialize(argc, argv);
+    
+    if (BA_ArgumentHandler_ContainsArgumentOrShort(SBR_BUILTINARGUMENTS_HELP, SBR_BUILTINARGUMENTS_HELP_SHORT, BA_BOOLEAN_FALSE)) {
+        BA_LOGGER_INFO("Arguments:\n%s\n"
+                       BA_ARGUMENTHANDLER_HELP_MESSAGE(SBR_BUILTINARGUMENTS_CURL_VERBOSE, SBR_BUILTINARGUMENTS_CURL_VERBOSE_SHORT, "cURL verbose messages\n")
+                       BA_ARGUMENTHANDLER_HELP_MESSAGE(SBR_BUILTINARGUMENTS_HELP, SBR_BUILTINARGUMENTS_HELP_SHORT, "Shows information about each argument\n"), BA_ArgumentHandler_GetHelpMessage());
+        return 0;
+    } 
+    
     SBR_Settings_Load();
     BA_LOGGER_DEBUG("Discord:\n"
                     "API version: %i\n"
