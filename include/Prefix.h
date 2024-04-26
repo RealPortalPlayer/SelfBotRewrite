1658fe3be395ce19356dc1ea36fe2692b41c954f
portalplayer <portalplayer@crime.su>
2024-04-25 21:26:51 -0400

    Bot: Added prefix

---
+++ include/Prefix.h
@@ -0,0 +1,9 @@
+// Purpose: Prefix getter
+// Created on: 4/25/24 @ 9:21 PM
+
+// Copyright (c) 2024, PortalPlayer <email@portalplayer.xyz>
+// Licensed under MIT <https://opensource.org/licenses/MIT>
+
+#pragma once
+
+const char* SBR_Prefix_Get(void);
