b92f85986b9619115263317874184c5728e05701
portalplayer <portalplayer@crime.su>
2024-04-27 16:50:28 -0400

    DiscordChannel: Actually added channel object

--- include/Discord/Objects/Channel.h
+++ include/Discord/Objects/Channel.h
@@ -6,8 +6,87 @@
 
 #pragma once
 
+#include <json_object.h>
+
 #include "Snowflake.h"
 
-// TODO: Channel object
+typedef enum {
+    SBR_DISCORDCHANNEL_TYPE_GUILD_TEXT,
+    SBR_DISCORDCHANNEL_TYPE_DIRECT_MESSAGE,
+    SBR_DISCORDCHANNEL_TYPE_GUILD_VOICE,
+    SBR_DISCORDCHANNEL_TYPE_GROUP_DIRECT_MESSAGE,
+    SBR_DISCORDCHANNEL_TYPE_GUILD_CATEGORY,
+    SBR_DISCORDCHANNEL_TYPE_GUILD_ANNOUNCEMENT,
+    SBR_DISCORDCHANNEL_TYPE_ANNOUNCEMENT_THREAD = 10,
+    SBR_DISCORDCHANNEL_TYPE_PUBLIC_THREAD,
+    SBR_DISCORDCHANNEL_TYPE_PRIVATE_THREAD,
+    SBR_DISCORDCHANNEL_TYPE_GUILD_STAGE_VOICE,
+    SBR_DISCORDCHANNEL_TYPE_GUILD_DIRECTORY,
+    SBR_DISCORDCHANNEL_TYPE_GUILD_FORUM,
+    SBR_DISCORDCHANNEL_TYPE_GUILD_MEDIA
+} SBR_DiscordChannel_Types;
+
+typedef enum {
+    SBR_DISCORDCHANNEL_CUSTOM_FLAG_NULL = 0,
+    SBR_DISCORDCHANNEL_CUSTOM_FLAG_NOT_SAFE_FOR_WORK = 1 << 0,
+    SBR_DISCORDCHANNEL_CUSTOM_FLAG_MANAGED = 1 << 1
+} SBR_DiscordChannel_CustomFlags;
+
+typedef enum {
+    SBR_DISCORDCHANNEL_AUTOMATIC_ARCHIVE_DURATION_60 = 60,
+    SBR_DISCORDCHANNEL_AUTOMATIC_ARCHIVE_DURATION_1440 = 1440,
+    SBR_DISCORDCHANNEL_AUTOMATIC_ARCHIVE_DURATION_4320 = 4320,
+    SBR_DISCORDCHANNEL_AUTOMATIC_ARCHIVE_DURATION_10080 = 10080
+} SBR_DiscordChannel_AutomaticArchiveDurations;
+
+typedef enum  {
+    SBR_DISCORDCHANNEL_OFFICIAL_FLAG_NULL = 0,
+    SBR_DISCORDCHANNEL_OFFICIAL_FLAG_PINNED_THREAD = 1 << 1,
+    SBR_DISCORDCHANNEL_OFFICIAL_FLAG_REQUIRE_THREAD_TAG = 1 << 4,
+    SBR_DISCORDCHANNEL_OFFICIAL_FLAG_HIDDEN_MEDIA_DOWNLOAD_OPTIONS = 1 << 15
+} SBR_DiscordChannel_OfficialFlags;
+
+typedef enum {
+    SBR_DISCORDCHANNEL_VIDEO_QUALITY_MODE_AUTO = 1,
+    SBR_DISCORDCHANNEL_VIDEO_QUALITY_MODE_FULL = 2
+} SBR_DiscordChannel_VideoQualityModes;
+
+typedef struct {
+    SBR_Snowflake* id;
+    SBR_DiscordChannel_Types type;
+    SBR_Snowflake* guildId;
+    int position;
+    // TODO: permission_overwrites
+    const char* name;
+    const char* topic;
+    SBR_DiscordChannel_CustomFlags customFlags;
+    SBR_Snowflake* lastMessageId;
+    int bitrate;
+    int userLimit;
+    int rateLimitPerUser;
+    // TODO: recipients
+    const char* iconHash;
+    SBR_Snowflake* ownerId;
+    SBR_Snowflake* applicationId;
+    SBR_Snowflake* parentId;
+    // TODO: last_pin_timestamp
+    // TODO: rtc_region
+    SBR_DiscordChannel_VideoQualityModes videoQualityMode;
+    int messageCount;
+    // TODO: thread_metadata
+    // TODO: member
+    SBR_DiscordChannel_AutomaticArchiveDurations defaultAutomaticArchiveDuration;
+    // TODO: permissions
+    SBR_DiscordChannel_OfficialFlags officialFlags;
+    int totalMessageSent;
+    // TODO: available_tags
+    // TODO: applied_tags
+    // TODO: default_reaction_emoji
+    int defaultThreadRateLimitPerUser;
+    int defaultSortOrder;
+    int defaultForumLayout;
+} SBR_DiscordChannel;
+
+SBR_DiscordChannel* SBR_DiscordChannel_Create(const json_object* unparsedJsonData);
 
 void SBR_DiscordChannel_Send(const SBR_Snowflake* id, const char* content);
--- source/Discord/Objects/Channel.c
+++ source/Discord/Objects/Channel.c
@@ -3,6 +3,46 @@
 
 #include "Discord/Objects/Channel.h"
 #include "Discord/API/Events.h"
+#include "Discord/Objects/ObjectCreatorHelpers.h"
+
+SBR_DiscordChannel* SBR_DiscordChannel_Create(const json_object* unparsedJsonData) {
+    SBR_OBJECTCREATORHELPERS_HEADER(SBR_DiscordChannel, "Discord channel");
+    SBR_OBJECTCREATORHELPERS_GET_SNOWFLAKE(id, "", REQUIRED, SAME);
+    SBR_OBJECTCREATORHELPERS_GET_INTEGER(type, "", REQUIRED, SAME);
+    SBR_OBJECTCREATORHELPERS_GET_SNOWFLAKE(guildId, "guild_id", OPTIONAL, NOT_SAME);
+    SBR_OBJECTCREATORHELPERS_GET_INTEGER(position, "", OPTIONAL, SAME);
+    // TODO: permission_overwrites
+    SBR_OBJECTCREATORHELPERS_GET_STRING(name, "", OPTIONAL, SAME);
+    SBR_OBJECTCREATORHELPERS_GET_STRING(topic, "", OPTIONAL, SAME);
+    SBR_OBJECTCREATORHELPERS_SET_BIT_ON_BOOLEAN(notSafeForWork, "nsfw", OPTIONAL, NOT_SAME, customFlags, SBR_DISCORDCHANNEL_CUSTOM_FLAG_NOT_SAFE_FOR_WORK);
+    SBR_OBJECTCREATORHELPERS_GET_SNOWFLAKE(lastMessageId, "last_message_id", OPTIONAL, NOT_SAME);
+    SBR_OBJECTCREATORHELPERS_GET_INTEGER(bitrate, "", OPTIONAL, SAME);
+    SBR_OBJECTCREATORHELPERS_GET_INTEGER(userLimit, "user_limit", OPTIONAL, NOT_SAME);
+    SBR_OBJECTCREATORHELPERS_GET_INTEGER(rateLimitPerUser, "rate_limit_per_user", OPTIONAL, NOT_SAME);
+    // TODO: recipients
+    SBR_OBJECTCREATORHELPERS_GET_STRING(iconHash, "icon", OPTIONAL, NOT_SAME);
+    SBR_OBJECTCREATORHELPERS_GET_SNOWFLAKE(ownerId, "owner_id", OPTIONAL, NOT_SAME);
+    SBR_OBJECTCREATORHELPERS_GET_SNOWFLAKE(applicationId, "application_id", OPTIONAL, NOT_SAME);
+    SBR_OBJECTCREATORHELPERS_SET_BIT_ON_BOOLEAN(managed, "", OPTIONAL, SAME, customFlags, SBR_DISCORDCHANNEL_CUSTOM_FLAG_MANAGED);
+    SBR_OBJECTCREATORHELPERS_GET_SNOWFLAKE(parentId, "parent_id", OPTIONAL, NOT_SAME);
+    // TODO: last_pin_timestamp
+    // TODO: rtc_region
+    SBR_OBJECTCREATORHELPERS_GET_INTEGER(videoQualityMode, "video_qulity_mode", OPTIONAL, NOT_SAME);
+    SBR_OBJECTCREATORHELPERS_GET_INTEGER(messageCount, "message_count", OPTIONAL, NOT_SAME);
+    // TODO: thread_metadata
+    // TODO: member
+    SBR_OBJECTCREATORHELPERS_GET_INTEGER(defaultAutomaticArchiveDuration, "default_auto_archive_duration", OPTIONAL, NOT_SAME);
+    // TODO: permissions
+    SBR_OBJECTCREATORHELPERS_GET_INTEGER(officialFlags, "flags", OPTIONAL, NOT_SAME);
+    SBR_OBJECTCREATORHELPERS_GET_INTEGER(totalMessageSent, "total_message_sent", OPTIONAL, NOT_SAME);
+    // TODO: available_tags
+    // TODO: applied_tags
+    // TODO: default_reaction_emoji
+    SBR_OBJECTCREATORHELPERS_GET_INTEGER(defaultThreadRateLimitPerUser, "default_thread_rate_limit_per_user", OPTIONAL, NOT_SAME);
+    SBR_OBJECTCREATORHELPERS_GET_INTEGER(defaultSortOrder, "default_sort_order", OPTIONAL, NOT_SAME);
+    SBR_OBJECTCREATORHELPERS_GET_INTEGER(defaultForumLayout, "default_forum_layout", OPTIONAL, NOT_SAME);
+    SBR_OBJECTCREATORHELPERS_FOOTER();
+}
 
 void SBR_DiscordChannel_Send(const SBR_Snowflake* id, const char* content) {
     SBR_DiscordAPIEvents_SendMessage(id, content);
