33b8af4d5f2d66a5c19b7e5fe8025aa705841420
portalplayer <portalplayer@crime.su>
2024-04-28 08:53:11 -0400

    DiscordAPI: Made it thread safe

--- source/Discord/API/API.c
+++ source/Discord/API/API.c
@@ -5,22 +5,36 @@
 #include <BaconAPI/Debugging/Assert.h>
 #include <BaconAPI/String.h>
 #include <json_tokener.h>
+#include <BaconAPI/Thread.h>
 
 #include "Discord/API/API.h"
 #include "Discord/API/Events.h"
 
 BA_Boolean SBR_DiscordAPI_Send(SBR_DiscordAPIEvent_Codes code, json_object* data, json_object** output, const SBR_DiscordAPIEvents_Variables* variables) {
+    static BA_Thread_Lock threadLock;
+    static BA_Boolean initialized = BA_BOOLEAN_FALSE;
+
+    if (!initialized) {
+        BA_ASSERT(BA_Thread_CreateLock(&threadLock), "Failed to create API message lock\n");
+
+        initialized = BA_BOOLEAN_TRUE;
+    }
+
+    BA_Thread_UseLock(&threadLock);
     BA_LOGGER_TRACE("Sending API: %i\n", code);
 
     if (!SBR_DiscordAPIEvent_IsCodeValid(code)) {
         BA_LOGGER_ERROR("Invalid code: %i\n", code);
+        BA_Thread_Unlock(&threadLock);
         return BA_BOOLEAN_FALSE;
     }
     
     *output = SBR_DiscordAPIEvents_Get(code)->action(data, variables);
 
-    if (*output == NULL)
+    if (*output == NULL) {
+        BA_Thread_Unlock(&threadLock);
         return BA_BOOLEAN_FALSE;
+    }
 
     {
         json_object* errorCode = json_object_object_get(*output, "code");
@@ -28,14 +42,17 @@ BA_Boolean SBR_DiscordAPI_Send(SBR_DiscordAPIEvent_Codes code, json_object* data
         
         if (json_object_get_type(errorCode) != json_type_null) {
             BA_LOGGER_ERROR("Discord API error (%i): %s\n", json_object_get_int(errorCode), json_object_get_string(errorMessage));
+            BA_Thread_Unlock(&threadLock);
             return BA_BOOLEAN_FALSE;
         }
 
         if (json_object_get_type(errorCode) != json_type_null) {
             BA_LOGGER_ERROR("Discord API error (HTTP): %s\n", json_object_get_string(errorMessage));
+            BA_Thread_Unlock(&threadLock);
             return BA_BOOLEAN_FALSE;
         }
     }
-    
+
+    BA_Thread_Unlock(&threadLock);
     return BA_BOOLEAN_TRUE;
 }
