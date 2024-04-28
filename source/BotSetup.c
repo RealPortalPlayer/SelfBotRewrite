db75703ebda05a14f8f9af7e9144f81b0b90b7f7
portalplayer <portalplayer@crime.su>
2024-04-28 10:34:15 -0400

    BotSetup: Added intents

--- source/BotSetup.c
+++ source/BotSetup.c
@@ -1,6 +1,12 @@
 // Copyright (c) 2024, PortalPlayer <email@portalplayer.xyz>
 // Licensed under MIT <https://opensource.org/licenses/MIT>
 
+#include "Discord/Intents.h"
+
 void SBR_BotSetup_Main(void) {
-    // TODO: Setup bot
+    SBR_DiscordIntents_Set(SBR_DISCORDINTENT_GUILD_MESSAGES |
+                           SBR_DISCORDINTENT_GUILD_MEMBERS |
+                           SBR_DISCORDINTENT_DIRECT_MESSAGES |
+                           SBR_DISCORDINTENT_GUILD_MODERATION |
+                           SBR_DISCORDINTENT_MESSAGE_CONTENT);
 }
