36538a51be58ceecbf8ed6f84c453a2315316e39
portalplayer <portalplayer@crime.su>
2024-04-28 10:56:29 -0400

    BotSetup: Added message sent stub

--- source/BotSetup.c
+++ source/BotSetup.c
@@ -1,7 +1,15 @@
 // Copyright (c) 2024, PortalPlayer <email@portalplayer.xyz>
 // Licensed under MIT <https://opensource.org/licenses/MIT>
 
+#include <BaconAPI/Logger.h>
+#include <BaconAPI/Math/Bitwise.h>
+
 #include "Discord/Intents.h"
+#include "Events.h"
+
+void SBR_BotSetup_MessageSent(SBR_DiscordMessage* message) {
+    // TODO
+}
 
 void SBR_BotSetup_Main(void) {
     SBR_DiscordIntents_Set(SBR_DISCORDINTENT_GUILD_MESSAGES |
@@ -9,4 +17,5 @@ void SBR_BotSetup_Main(void) {
                            SBR_DISCORDINTENT_DIRECT_MESSAGES |
                            SBR_DISCORDINTENT_GUILD_MODERATION |
                            SBR_DISCORDINTENT_MESSAGE_CONTENT);
+    SBR_Events_SetMessageSentAction(&SBR_BotSetup_MessageSent);
 }
