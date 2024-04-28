871651fbb580d51cc2694f0349c8396f31a69383
portalplayer <portalplayer@crime.su>
2024-04-28 12:11:24 -0400

    Events: Added trace logs

--- source/Events.c
+++ source/Events.c
@@ -1,6 +1,8 @@
 // Copyright (c) 2024, PortalPlayer <email@portalplayer.xyz>
 // Licensed under MIT <https://opensource.org/licenses/MIT>
 
+#include <BaconAPI/Logger.h>
+
 #include "Events.h"
 
 #define SBR_EVENTS_SETUP_EVENT_FUNCTIONS(name, ...) \
@@ -9,8 +11,11 @@ void SBR_Events_Set ## name ## Action(SBR_Events_ ## name ## Action action) { \
     sbrEvents ## name = action;                     \
 }                                                   \
 void SBR_Events_ ## name(__VA_ARGS__) {             \
-    if (sbrEvents ## name == NULL)                  \
+    BA_LOGGER_TRACE("Calling custom event: " #name "\n"); \
+    if (sbrEvents ## name == NULL) {                \
+        BA_LOGGER_TRACE("Custom event function is null\n"); \
         return;                                     \
+    }                                               \
     sbrEvents ## name
 
 SBR_EVENTS_SETUP_EVENT_FUNCTIONS(MessageSent, SBR_DiscordMessage* message)(message);}
