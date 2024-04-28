857122c76c98549498475b878c9385970c7eeb1f
portalplayer <portalplayer@crime.su>
2024-04-28 10:51:25 -0400

    Bot: Allow setting custom events

---
+++ include/Events.h
@@ -0,0 +1,16 @@
+// Purpose: Bot events
+// Created on: 4/28/24 @ 10:36 AM
+
+// Copyright (c) 2024, PortalPlayer <email@portalplayer.xyz>
+// Licensed under MIT <https://opensource.org/licenses/MIT>
+
+#pragma once
+
+#include "Discord/Objects/Message.h"
+
+#define SBR_EVENTS_SETUP_EVENT(name, ...) \
+typedef void (*SBR_Events_ ## name ## Action)(__VA_ARGS__); \
+void SBR_Events_ ## name(__VA_ARGS__); \
+void SBR_Events_Set ## name ## Action(SBR_Events_ ## name ## Action action)
+
+SBR_EVENTS_SETUP_EVENT(MessageSent, SBR_DiscordMessage* message);
---
+++ source/Events.c
@@ -0,0 +1,16 @@
+// Copyright (c) 2024, PortalPlayer <email@portalplayer.xyz>
+// Licensed under MIT <https://opensource.org/licenses/MIT>
+
+#include "Events.h"
+
+#define SBR_EVENTS_SETUP_EVENT_FUNCTIONS(name, ...) \
+static SBR_Events_ ## name ## Action sbrEvents ## name = NULL; \
+void SBR_Events_Set ## name ## Action(SBR_Events_ ## name ## Action action) { \
+    sbrEvents ## name = action;                     \
+}                                                   \
+void SBR_Events_ ## name(__VA_ARGS__) {             \
+    if (sbrEvents ## name == NULL)                  \
+        return;                                     \
+    sbrEvents ## name
+
+SBR_EVENTS_SETUP_EVENT_FUNCTIONS(MessageSent, SBR_DiscordMessage* message)(message);}
