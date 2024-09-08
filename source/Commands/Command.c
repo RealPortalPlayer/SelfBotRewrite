6ddfffe439b2c68ed25f8171fd85113e806f866a
portalplayer <portalplayer@crime.su>
2024-09-08 15:06:25 -0400

    Bot: Allow disabling command types

--- source/Commands/Command.c
+++ source/Commands/Command.c
@@ -5,8 +5,10 @@
 #include <BaconAPI/Debugging/Assert.h>
 #include <BaconAPI/String.h>
 #include <BaconAPI/Math/Bitwise.h>
+#include <BaconAPI/ArgumentHandler.h>
 
 #include "Commands/Command.h"
+#include "BuiltInArguments.h"
 
 static BA_DynamicArray* sbrCommandRegistered;
 static BA_Boolean sbrCommandInitialized = BA_BOOLEAN_FALSE;
@@ -25,6 +27,16 @@ void SBR_Command_Initialize(void) {
 void SBR_Command_Register(const char* name, const char* description, const char* categoryName, SBR_Command_Type type, SBR_Command_Action Action) {
     // TODO: Checks
 
+    static BA_Boolean terminalCommandsDisabled = -1;
+    static BA_Boolean slashCommandsDisabled = -1;
+    static BA_Boolean classicCommandsDisabled = -1;
+
+    if (terminalCommandsDisabled == -1) {
+        terminalCommandsDisabled = BA_ArgumentHandler_ContainsArgumentOrShort(SBR_BUILTINARGUMENTS_DISABLE_TERMINAL_COMMANDS, SBR_BUILTINARGUMENTS_DISABLE_TERMINAL_COMMANDS_SHORT, BA_BOOLEAN_FALSE);
+        slashCommandsDisabled = BA_ArgumentHandler_ContainsArgumentOrShort(SBR_BUILTINARGUMENTS_DISABLE_SLASH_COMMANDS, SBR_BUILTINARGUMENTS_DISABLE_SLASH_COMMANDS_SHORT, BA_BOOLEAN_FALSE);
+        classicCommandsDisabled = BA_ArgumentHandler_ContainsArgumentOrShort(SBR_BUILTINARGUMENTS_DISABLE_CLASSIC_COMMANDS, SBR_BUILTINARGUMENTS_DISABLE_CLASSIC_COMMANDS_SHORT, BA_BOOLEAN_FALSE);
+    }
+    
     SBR_Command* command = malloc(sizeof(SBR_Command));
 
     BA_ASSERT(command != NULL, "Failed to allocate memory for command: %s\n", name);
@@ -35,6 +47,20 @@ void SBR_Command_Register(const char* name, const char* description, const char*
     command->type = type;
     command->Action = Action;
 
+#define SBR_COMMAND_CHECK_FOR_DISABLED_TYPE(check, removedType) \
+do {                                                        \
+    if (check && BA_BITWISE_IS_BIT_SET(command->type, removedType)) { \
+        BA_BITWISE_UNSET_BIT(command->type, removedType);   \
+        if (command->type == 0) {                           \
+            free(command);                                  \
+            return;                                         \
+        }                                                   \
+    }                                                       \
+} while (BA_BOOLEAN_FALSE)
+
+    SBR_COMMAND_CHECK_FOR_DISABLED_TYPE(terminalCommandsDisabled, SBR_COMMAND_TYPE_TERMINAL);
+    SBR_COMMAND_CHECK_FOR_DISABLED_TYPE(slashCommandsDisabled, SBR_COMMAND_TYPE_SLASH);
+    SBR_COMMAND_CHECK_FOR_DISABLED_TYPE(classicCommandsDisabled, SBR_COMMAND_TYPE_CLASSIC);
     BA_ASSERT(BA_DynamicArray_AddElementToLast(sbrCommandRegistered, command), "Failed to add registered command\n");
 }
 
