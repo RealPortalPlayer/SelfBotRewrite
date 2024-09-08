c823959d54fe5e5f4d9648cb5850b8818b549ccc
portalplayer <portalplayer@crime.su>
2024-09-08 09:57:41 -0400

    help: Show categories by default

--- source/Commands/Commands.c
+++ source/Commands/Commands.c
@@ -4,9 +4,11 @@
 #include <BaconAPI/Logger.h>
 #include <BaconAPI/String.h>
 #include <BaconAPI/Math/Bitwise.h>
+#include <BaconAPI/Storage/DynamicDictionary.h>
 
 #include "Commands/Commands.h"
 #include "Commands/Command.h"
+#include "Commands/Category.h"
 
 #define SBR_COMMANDS_CREATE_COMMAND_HEADER(name) void SBR_Commands_ ## name(const SBR_Command* this, SBR_DiscordMessage* message)
 #define SBR_COMMANDS_REGISTER(name, description, category, type) SBR_Command_Register(#name, description, category, type, (SBR_Command_Action) &SBR_Commands_ ## name)
@@ -28,16 +30,14 @@ SBR_COMMANDS_CREATE_COMMAND_HEADER(test) {
 }
 
 SBR_COMMANDS_CREATE_COMMAND_HEADER(help) {
-    const BA_DynamicArray* registeredCommands = SBR_Command_GetAll();
+    const BA_DynamicDictionary* registeredCategories = SBR_Category_GetAll();
     SBR_EmbedCreator_Embed* embed = SBR_EmbedCreator_Create();
 
-    for (int i = 0; i < registeredCommands->used; i++) {
-        SBR_Command* command = BA_DYNAMICARRAY_GET_ELEMENT_POINTER(SBR_Command, registeredCommands, i);
-
-        if (!BA_BITWISE_IS_BIT_SET(command->type, SBR_COMMAND_TYPE_CLASSIC))
-            continue;
-
-        SBR_EmbedCreator_AddField(embed, command->name, command->description, BA_BOOLEAN_TRUE);
+    for (int i = 0; i < registeredCategories->keys.used; i++) {
+        const char* name = registeredCategories->keys.internalArray[i];
+        const char* description = SBR_Category_GetDescription(name);
+        
+        SBR_EmbedCreator_AddField(embed, name, description != NULL ? description : "(NULL)", BA_BOOLEAN_TRUE);
     }
 
     SBR_DiscordChannel_Send(message->channel, "", embed);
