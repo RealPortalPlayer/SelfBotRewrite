65772cf127ddd96ead4181b69d85d54d9e1f1bfb
portalplayer <portalplayer@crime.su>
2024-09-08 11:15:27 -0400

    Commands: Added `debug` command

--- source/Commands/Commands.c
+++ source/Commands/Commands.c
@@ -15,11 +15,13 @@
 
 SBR_COMMANDS_CREATE_COMMAND_HEADER(test);
 SBR_COMMANDS_CREATE_COMMAND_HEADER(help);
+SBR_COMMANDS_CREATE_COMMAND_HEADER(debug);
 
 void SBR_Commands_Register(void) {
     BA_LOGGER_INFO("Registering all commands\n");
     SBR_COMMANDS_REGISTER(test, "Testing command", "misc", SBR_COMMAND_TYPE_CLASSIC);
     SBR_COMMANDS_REGISTER(help, "Information about each command", "information", SBR_COMMAND_TYPE_CLASSIC);
+    SBR_COMMANDS_REGISTER(debug, "Shows information about the bot", "information", SBR_COMMAND_TYPE_CLASSIC);
 }
 
 SBR_COMMANDS_CREATE_COMMAND_HEADER(test) {
@@ -42,4 +44,38 @@ SBR_COMMANDS_CREATE_COMMAND_HEADER(help) {
 
     SBR_DiscordChannel_Send(message->channel, "", embed);
     SBR_EmbedCreator_Free(embed);
+}
+
+SBR_COMMANDS_CREATE_COMMAND_HEADER(debug) {
+    char* newMessage = BA_String_Copy("Terminal commands: %i\n"
+                                      "Classic categories: %i\n"
+                                      "Classic commands: %i\n"
+                                      "Slash commands: %i\n"
+                                      "Version: %s");
+
+    int terminalCommands = 0;
+    int classicCommands = 0;
+    int slashCommands = 0;
+
+    {
+        const BA_DynamicArray* registeredCommands = SBR_Command_GetAll();
+
+        for (int i = 0; i < registeredCommands->used; i++) {
+            SBR_Command* command = BA_DYNAMICARRAY_GET_ELEMENT_POINTER(SBR_Command, registeredCommands, i);
+
+            if (BA_BITWISE_IS_BIT_SET(command->type, SBR_COMMAND_TYPE_TERMINAL))
+                terminalCommands++;
+            else if (BA_BITWISE_IS_BIT_SET(command->type, SBR_COMMAND_TYPE_CLASSIC))
+                classicCommands++;
+            else if (BA_BITWISE_SET_BIT(command->type, SBR_COMMAND_TYPE_SLASH))
+                slashCommands++;
+        }
+    }
+    
+    SBR_DiscordChannel_Send(message->channel, BA_String_Format(&newMessage, terminalCommands,
+                                                                            SBR_Category_GetAll()->keys.used,
+                                                                            classicCommands,
+                                                                            slashCommands,
+                                                                            SBR_VERSION), NULL);
+    free(newMessage);
 }
