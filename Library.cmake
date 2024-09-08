f5c61bf634739ac041a8a7f1d8b39caf14b5bf39
portalplayer <portalplayer@crime.su>
2024-09-08 14:54:44 -0400

    Build Script: Removed useless define

--- Library.cmake
+++ Library.cmake
@@ -7,7 +7,6 @@ target_include_directories(libSelfBotRemake PUBLIC include)
 target_compile_definitions(libSelfBotRemake PUBLIC SBR_DISCORD_API_VERSION=${DISCORD_API_VERSION}
                                                    SBR_DISCORD_ROOT_URL="${DISCORD_ROOT_URL}"
                                                    SBR_DISCORD_API_URL="${DISCORD_API_URL}"
-                                                   SBR_DISCORD_WEBSOCKET_URL="${DISCORD_WEBSOCKET_URL}"
                                                    SBR_DISCORD_DEPRECATED_API_VERSION=${DISCORD_DEPRECATED_API_VERSION}
                                                    SBR_DISCORD_CDN_URL="${DISCORD_CDN_URL}"
                                                    SBR_STATIC
