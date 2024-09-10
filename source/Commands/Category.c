41b6746be672c7b8038e5215e8c9be1bbf59597e
portalplayer <portalplayer@crime.su>
2024-09-10 02:37:25 -0400

    Category: Use the new memory system

--- source/Commands/Category.c
+++ source/Commands/Category.c
@@ -6,16 +6,19 @@
 #include <BaconAPI/Logger.h>
 
 #include "Commands/Category.h"
+#include "Storage/DynamicDictionary.h"
+#include "Storage/DynamicArray.h"
+#include "Memory.h"
 
-static BA_DynamicDictionary* sbrCategoryRegistered;
+static BA_DynamicDictionary sbrCategoryRegistered;
 static BA_Boolean sbrCategoryInitialized = BA_BOOLEAN_FALSE;
 
 const char* SBR_Category_GetDescription(const char* name) {
-    return BA_DynamicDictionary_GetElementValueViaKey(sbrCategoryRegistered, name, sizeof(char) * (strlen(name) + 1));
+    return BA_DynamicDictionary_GetElementValueViaKey(&sbrCategoryRegistered, name, sizeof(char) * (strlen(name) + 1));
 }
 
 const BA_DynamicDictionary* SBR_Category_GetAll(void) {
-    return sbrCategoryRegistered;
+    return &sbrCategoryRegistered;
 }
 
 void SBR_Category_Initialize(void) {
@@ -23,14 +26,14 @@ void SBR_Category_Initialize(void) {
     BA_LOGGER_INFO("Initializing categories\n");
 
     sbrCategoryInitialized = BA_BOOLEAN_TRUE;
-    sbrCategoryRegistered = malloc(sizeof(BA_DynamicDictionary));
 
-    BA_ASSERT(sbrCategoryRegistered != NULL, "Failed to allocate memory for registered categories\n");
-    BA_ASSERT(BA_DynamicDictionary_Create(sbrCategoryRegistered, 10), "Failed to initialize registered categories array\n");
+    BA_ASSERT(SBR_DynamicDictionary_Create(&sbrCategoryRegistered, 10), "Failed to initialize registered categories array\n");
 }
 
 void SBR_Category_Register(const char* name, const char* description) {
-    BA_ASSERT(BA_DynamicDictionary_AddElementToLast(sbrCategoryRegistered, (void*) name, (void*) description), "Failed to add registered category\n");
+    SBR_DynamicArray_CheckResize(&sbrCategoryRegistered.keys);
+    SBR_DynamicArray_CheckResize(&sbrCategoryRegistered.values);
+    BA_ASSERT(BA_DynamicDictionary_AddElementToLast(&sbrCategoryRegistered, (void*) name, (void*) description), "Failed to add registered category\n");
 }
 
 void SBR_Category_Destroy(void) {
@@ -39,7 +42,6 @@ void SBR_Category_Destroy(void) {
 
     sbrCategoryInitialized = BA_BOOLEAN_FALSE;
 
-    free(sbrCategoryRegistered->keys.internalArray);
-    free(sbrCategoryRegistered->values.internalArray);
-    free(sbrCategoryRegistered);
+    BA_Memory_Deallocate(sbrCategoryRegistered.keys.internalArray, sizeof(void*) * sbrCategoryRegistered.keys.size, SBR_MEMORY_TYPE_DYNAMIC_ARRAY);
+    BA_Memory_Deallocate(sbrCategoryRegistered.values.internalArray, sizeof(void*) * sbrCategoryRegistered.values.size, SBR_MEMORY_TYPE_DYNAMIC_ARRAY);
 }
